42353fa40dce286e673d31a67e186f6045a87bf3
daniel-chowdhury <danielfchowdhury@gmail.com>
2022-07-18 22:44:15 -0400

    Week 2, day 1, lab is completed (introduction to functions, folder lab_function-drills)

---
+++ day_1/StringPractice.js
@@ -0,0 +1,40 @@
+//SOME AWESOME METHODS FOR STRINGS
+let practice_with_some_strings = "Hello, world"
+//obtaining portions of a string
+//substring and return "ell" ----->  
+shorter = practice_with_some_strings.substring(1, 4)
+console.log(shorter)
+//slice and return "worl" -----> 
+console.log(practice_with_some_strings.slice(7, 11))
+//split and return an array of both words ----->
+console.log(practice_with_some_strings.split(", ")) 
+array = practice_with_some_strings.split(", ")
+//Bonus: what method can take an array of words and put them back together
+console.log(array.join())
+//searching a string
+
+//indexOf "H" ---->
+console.log(practice_with_some_strings.slice())
+//lastIndexOf "l" ----> 
+
+//boolean searching funtions
+//startsWith "Hello" gives us?---> 
+//endsWith "!" ---> 
+
+//creating modified version of a string
+//replace "llo" with "ya" ---> "Heya, world"
+//toLowerCase ---> take a guess lol
+//toUpperCase ---> take another guess
+
+//inspecting individual characters of a string
+//charAt 0 ---> 
+//charCodeAt 0  --->
+//Bonus: find the range of lower and uppercase letters
+
+//space trim funtctions
+//bonus mess with these methods
+let test = "   test "
+//trim test --->
+//Bonus: mess with the other trims
+//trimStart and trimEnd
+console.log(true + false)
